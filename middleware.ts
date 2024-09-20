e8c12f3066a7e34a7592ae68cdc82f2fe3c21e7e
Jethro Baring <baring.jeth@gmail.com>
2024-09-20 23:04:11 +0800

    test

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,8 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export async function middleware(request: NextRequest) {
+  console.log(request)
+  console.log("HANNAH")
   const cookie = cookies().get('session');
 
   if (!cookie) {
